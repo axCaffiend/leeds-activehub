f034d3e15f00c94a6f5dc41c0b34a4f94eb2defe
axCaffiend <axfulcher@outlook.com>
2024-11-11 10:01:51 +0000

    (browse-activities)(main-style): Added main search form and filters - Added no UI Slider plugin for price range multi range slider input - Overridden CSS for noUISlider under form controls - Divided main form into sub sections for key info search (visible at page top) and more filters (will become a modal)

---
+++ js/no-ui-slider-config.js
@@ -0,0 +1,43 @@
+console.log("no-ui-slider-config running");
+
+const priceMinNumInput = document.getElementById("price-min");
+const priceMaxNumInput = document.getElementById("price-max");
+const priceRange = document.getElementById('price-range');
+
+
+noUiSlider.create(priceRange, {
+    start: [1, 50],
+    step: 1,
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 50
+    },
+    pips: {
+        mode: 'count',
+        values: 6,
+        density: 4
+    }
+});
+
+priceRange.noUiSlider.on('update', function (values, handle) {
+
+    // Where 'handle' is the index of the handle that caused the event on the slider i.e. value in initialised options 'start:[1, 100]'.
+    let value = values[handle];
+
+    if (handle==1) {
+        // Second handle
+        priceMaxNumInput.value = value;
+    } else { 
+        // First handle
+        priceMinNumInput.value = value;
+    }
+});
+
+priceMaxNumInput.addEventListener('change', function () {
+    priceRange.noUiSlider.set([null, this.value]);
+});
+
+priceMinNumInput.addEventListener('change', function () {
+    priceRange.noUiSlider.set([this.value, null], true, true);
+});
